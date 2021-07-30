0ed72077f0ae7414278cf0f5a7d5596537825b68
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-07-30 17:41:55 +0300

    feat: create function pow JS-Part1:TASK-2.15-4

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,3 +1,12 @@
-function min(a,b){
-    return a>b?b:a
+function pow(x, n) {
+    return x ** n
+}
+
+let x = prompt("input x", '');
+let n = prompt("input n", '');
+
+if (n < 1) {
+    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+    alert(pow(x, n));
 }
