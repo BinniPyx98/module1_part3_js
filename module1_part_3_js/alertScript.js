26f0f6a8a96bb1eac30d3f36ed0d7c9fd44c2c23
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-07-30 17:39:51 +0300

    feat: create function min(a,b) JS-Part1:TASK-2.15-3

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,6 +1,3 @@
-function checkAge(age){
-return age>18?true:confirm('Родители разрешили?')
-}
-function checkAge2(age){
-    return age>18||confirm('Родители разрешили?')
+function min(a,b){
+    return a>b?b:a
 }
