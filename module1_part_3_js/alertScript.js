ec997dc114da5eb65635575a75fd17077a162cb4
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-07-30 18:18:11 +0300

    feat: change function to arrow function JS-Part1:TASK-2.17-1

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,12 +1,14 @@
-function pow(x, n) {
-    return x ** n
+let ask = (question, yes, no) => {
+    if (confirm(question)) yes()
+    else no();
 }
 
-let x = prompt("input x", '');
-let n = prompt("input n", '');
-
-if (n < 1) {
-    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
-} else {
-    alert(pow(x, n));
-}
+ask(
+    "Вы согласны?",
+    () => {
+        alert("Вы согласились.");
+    },
+    () => {
+        alert("Вы отменили выполнение.");
+    }
+);
