5389137ecc5dbe05b09fc1a202fc1eaf79715cc9
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-07-30 14:26:45 +0300

    eat: learn ternary opiration learnJS-Part1:TASK-2.10-4

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,10 +1,2 @@
-let a=prompt("input number?","")
- if(Number(a)>0){
-     alert(1)
- }
- else if (Number(a)<0){
-     alert(-1)
- }
- else{
-     alert(0)
- }
+let result
+(a+b<4)?result="Мало":result="Много"
