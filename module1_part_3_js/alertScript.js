fbbcf7d82fff9192fc51d4e5ca6af146d300fa7f
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-07-30 15:52:34 +0300

    feat: console log number%2==0 learnJS-Part1:TASK-2.12-4

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,19 +1,4 @@
-let userName = prompt("Кто там?", '');
-
-if (userName == 'Админ') {
-    
-    let pass = prompt('Пароль?', '');
-    
-    if (pass == 'Я главный') {
-        alert('Здравствуйте!');
-    } else if (pass == '' || pass == null) {
-        alert('Отменено');
-    } else {
-        alert('Неверный пароль');
-    }
-    
-} else if (userName == '' || userName == null) {
-    alert('Отменено');
-} else {
-    alert("Я вас не знаю");
+for(let i=2;i<10;i++){
+    if(!(i%2==0))continue
+    console.log(i)
 }
