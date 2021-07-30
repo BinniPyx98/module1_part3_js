d287774b97cfab80f6a74ef27fb95e833317250e
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-07-30 17:35:25 +0300

    feat: create function checkAge JS-Part1:TASK-2.15-2

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,14 +1,6 @@
-const number=+prompt('input 3>number>0','')
-switch(number){
-    case 0:
-        alert("You input 0")
-        break;
-    case 1:
-        alert("You input 1")
-        break;
-    case 2:
-    case 3:
-        alert('You input 2 or 3')
-        break
-    
+function checkAge(age){
+return age>18?true:confirm('Родители разрешили?')
+}
+function checkAge2(age){
+    return age>18||confirm('Родители разрешили?')
 }
