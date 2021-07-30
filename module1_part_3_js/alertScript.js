0328aa7b2dfa80f67037e2ff05c930d7d38a3884
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-07-30 17:10:29 +0300

    feat:change if to switch learnJS-Part1:TASK-2.14-2

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,9 +1,14 @@
-if(browser==='Edge'){
-    alert("You've got the Edge!");
-}
-else if(browser==='Chrome'||'Firefox'||'Safari'||'Opera'){
-    alert("Okay we support these browsers too")
-}
-else{
-    alert("We hope that this page looks ok!")
+const number=+prompt('input 3>number>0','')
+switch(number){
+    case 0:
+        alert("You input 0")
+        break;
+    case 1:
+        alert("You input 1")
+        break;
+    case 2:
+    case 3:
+        alert('You input 2 or 3')
+        break
+    
 }
