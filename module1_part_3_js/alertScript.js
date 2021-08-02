18b7c974f16182b47c86b36e0fb38252a9544c92
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-02 11:05:25 +0300

    feat: create function isEmpty learnJS-Part1:TASK-4.1-2

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,5 +1,6 @@
-let user={}
-user.name="John"
-user.surname="Smith"
-user.name="Pete"
-delete user.name
+let isEmpty=(obj)=>{
+    for(let key in obj){
+        return false
+    }
+    return true
+}s
