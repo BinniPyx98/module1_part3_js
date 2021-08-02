a187337346d67460dbfc08cca579c46e87cbe114
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-02 10:54:29 +0300

    feat:create object learnJS-Part1:TASK-4.1-1

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,14 +1,5 @@
-let ask = (question, yes, no) => {
-    if (confirm(question)) yes()
-    else no();
-}
-
-ask(
-    "Вы согласны?",
-    () => {
-        alert("Вы согласились.");
-    },
-    () => {
-        alert("Вы отменили выполнение.");
-    }
-);
+let user={}
+user.name="John"
+user.surname="Smith"
+user.name="Pete"
+delete user.name
