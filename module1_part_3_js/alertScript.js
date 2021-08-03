b60847ae4bc558c11e93f161a56c5f86f82490fc
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-03 18:39:51 +0300

    feat: methods array upperCase

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,21 +1,15 @@
-function sumInput() {
-    
-    let numbers = [];
-    
-    while (true) {
-        
-        let value = prompt("Введите число", 0);
-        
-        if (value === "" || value === null || !isFinite(value)) break;
-        
-        numbers.push(+value);
+let camelize=(str)=>{
+    let array=str.split()
+    for(let i=0;i<array.length;i++){
+        if(array[i]==="-"){
+            upChar=array.splice(i+1,1)
+            array.splice(i,1,upChar.toUpperCase())
+            array[i+1]=array[i+1].toUpperCase()
+        }
     }
-    
-    let sum = 0;
-    for (let number of numbers) {
-        sum += number;
-    }
-    return sum;
-}
-
-alert( sumInput() );
+    return array.join()
+        }
+        
+        let a='background-color'
+let b=camelize(a)
+console.log(b)
