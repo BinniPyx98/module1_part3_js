3bb7f246d21465bffe0cdfcbccd5f1364870b600
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-03 17:18:51 +0300

    feat:learn array methods learnJS-Part1:TASK-5.4-2

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,3 +1,6 @@
-function extractCurrencyValue(str) {
-    return +str.slice(1);
-}
+let styles=['Джаз',"Блюз"]
+styles.push( "Рок-н-Рол")
+styles[Math.floor((styles.length-1)/2)]='Classica'
+console.log(styles.shift())
+styles.unshift('raggi')
+styles.unshift('Rap')
