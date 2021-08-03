e8571c636e0519e14a11ee42e7cee3e16ed31d19
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-03 16:40:46 +0300

    feat:create fubction checkSpam learnJS-Part1:TASK-5.2-2

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,9 +1,9 @@
-let ucFirst=(str)=>{
-    if(str.length!=0){
-    let newStr=str[0].toUpperCase()
-    for(let i=1;i<str.length;i++){
-        newStr+=str[i]
+let checkSpam=(str)=>{
+    let newStr=str.toUpperCase()
+    if(newStr==='VIAGRA'||'XXX'){
+        return true
     }
-    return newStr
+    else{
+        return false
     }
 }
