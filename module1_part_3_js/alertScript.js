6ec63ef66859bbc9aae12e85839672ab42a616f7
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-04 16:42:41 +0300

    feat: uniqueArr learnJS-Part1:TASK-5.7-1

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,19 +1,10 @@
-function Calculator(){
-    this.calculator=function (str){
-        let  operrator=str.slice(' ')
-        let result= this.methods[operrator[1]](Number(operrator[0]),Number(operrator[2]))
-        console.log(result)
-    }
-    this.methods={
-        '+':(a,b)=>{return a+b},
-        '-':(a,b)=>{return a-b}
-    }
-    this.addMethod=function (str,func){
-        this.methods[str]=func
-    }
+function unique(arr) {
+    let uniqueArr= new Set(arr)
+    return uniqueArr
 }
 
-let cal=new Calculator
-// cal.calculator('1+2')
-cal.addMethod("*",(a,b)=>a*b)
-cal.calculator("2*2")
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+console.log( unique(values) ); // Hare,Krishna,:-O
