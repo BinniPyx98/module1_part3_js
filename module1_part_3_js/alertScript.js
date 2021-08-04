744a95b11c18034df696f2e3512d09b93ec9203f
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-04 13:55:16 +0300

    feat: users name learnJS-Part1:TASK-5.5-5

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,14 +1,12 @@
+let vasya = {name: "aaa", age: 25};
+let petya = {name: "ddd", age: 30};
+let masha = {name: "ccc", age: 28};
 
-let copySorted=(arr)=>{
-    
-    let newArr=[...arr]
-    return newArr.sort()
-    
-    
+let users = [vasya, petya, masha];
+
+let names = []
+for (let i = 0; i < users.length; i++) {
+    names.push(users[i].name)
 }
 
-let arr = ["HTML", "JavaScript", "CSS"];
-
-let sorted = copySorted(arr);
-
-console.log( arr ); // CSS, HTML, JavaScript
+console.log(names); // Вася, Петя, Маша
