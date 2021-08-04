fab7f74628e874c05b856a9942844f70d5d6648b
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-04 18:43:28 +0300

    feat: getDay learnJS-Part1:TASK-5.11-2

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,2 +1,31 @@
-let date= new Date(2012,1,20,3,12)
-console.log(date)
+let getWeekDay = (date) => {
+    switch (date.getDay()) {
+        case 0:
+            return "ВС"
+            break;
+        case 1:
+            return "ПН"
+            break;
+        case 2:
+            return "ВТ"
+            break;
+        case 3:
+            return "СР"
+            break;
+        case 4:
+            return "Чт"
+            break;
+        case 5:
+            return "Пт"
+            break;
+            break;
+        case 6:
+            return "СБ"
+            break;
+        
+    }
+}
+
+
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+console.log(getWeekDay(date));        // нужно вывести "
