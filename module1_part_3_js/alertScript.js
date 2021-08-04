ac0f8d6e57f7f0fce10f42aece704c37f0a1e44f
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-04 16:06:41 +0300

    feat: create calculator learnJS-Part1:TASK-5.5-7

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,16 +1,19 @@
-let unique = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 1; j < arr.length; j++) {
-            if(arr[i]===arr[j]){
-                arr.splice(i,1)
-            }
-        }
+function Calculator(){
+    this.calculator=function (str){
+        let  operrator=str.slice(' ')
+        let result= this.methods[operrator[1]](Number(operrator[0]),Number(operrator[2]))
+        console.log(result)
     }
-    
+    this.methods={
+        '+':(a,b)=>{return a+b},
+        '-':(a,b)=>{return a-b}
+    }
+    this.addMethod=function (str,func){
+        this.methods[str]=func
+    }
 }
 
-let strings = ["кришна", "кришна", "харе", "харе",
-    "харе", "харе", "кришна", "кришна", ":-O"
-];
-unique(strings)
-console.log(strings);
+let cal=new Calculator
+// cal.calculator('1+2')
+cal.addMethod("*",(a,b)=>a*b)
+cal.calculator("2*2")
