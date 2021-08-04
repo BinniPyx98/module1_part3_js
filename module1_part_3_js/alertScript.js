874eeec21d28697bed1d9284b9c9ad1862525f8f
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-04 14:16:21 +0300

    feat: learn array method sort array objects learnJS-Part1:TASK-5.5-6

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,12 +1,13 @@
+let sortByAge=(arr)=>{
+    arr.sort((a,b)=>a.age-b.age)
+}
+
 let vasya = {name: "aaa", age: 25};
 let petya = {name: "ddd", age: 30};
 let masha = {name: "ccc", age: 28};
 
 let users = [vasya, petya, masha];
 
-let names = []
-for (let i = 0; i < users.length; i++) {
-    names.push(users[i].name)
-}
+sortByAge(users)
 
-console.log(names); // Вася, Петя, Маша
+console.log(users); // Вася, Петя, Маша
