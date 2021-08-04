df84a0be92d662a4beda298b62b117f4f754a4ca
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-04 17:23:09 +0300

    feat: del anagram learnJS-Part1:TASK-5.7-2

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,10 +1,23 @@
-function unique(arr) {
-    let uniqueArr= new Set(arr)
-    return uniqueArr
+let aclean = (inputArr) => {
+    
+    let arr = [...inputArr]
+    let newSetArr = new Set
+    let testArr =null
+    let result = []
+    
+    for (let i = 0; i < arr.length; i++) {
+        testArr = arr[i].toUpperCase().split("").sort().join()
+        
+        if (!newSetArr.has(testArr)) {
+            newSetArr.add(testArr)
+            result.push(inputArr[i])
+        }
+    }
+    
+    console.log(newSetArr)
+    return result
 }
 
-let values = ["Hare", "Krishna", "Hare", "Krishna",
-    "Krishna", "Krishna", "Hare", "Hare", ":-O"
-];
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 
-console.log( unique(values) ); // Hare,Krishna,:-O
+console.log(aclean(arr)); // "nap,teachers,ear" or "PAN,cheaters,era"
