f796f2ea1de90c85bc17539c64e3cc5f0fb8fe5b
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-08-05 17:15:09 +0300

    feat: create function printNumbers learnJS-Part1:TASK-6.8-1

--- module1_part_3_js/alertScript.js
+++ module1_part_3_js/alertScript.js
@@ -1,16 +1,24 @@
-function makeCounter() {
-    
-    function counter() {
-        return counter.count++;
-    };
-    counter.set=(value)=>{counter.count=value}
-    counter.decrease=()=>{return counter.count--}
-    
-    counter.count = 0;
-    
-    return counter;
+// let printNumbers=(from, to)=> {
+//     let current = from
+//     let inter = setInterval(() => {
+//         if (from == to) {
+//             clearInterval(inter)
+//         } else {
+//             console.log(from)
+//             from++
+//         }
+//     }, 1000)
+// }
+let printNumbers=(from,to)=>{
+    let current=from
+    let time=setTimeout(function test(){
+        if(current===to){
+            clearTimeout(time)
+        }else {
+            console.log(current)
+            current++
+           setTimeout(test,1000)
+        }
+    },1000)
 }
-
-let counter = makeCounter();
-
-counter.count = 10;
+printNumbers(5, 10);
